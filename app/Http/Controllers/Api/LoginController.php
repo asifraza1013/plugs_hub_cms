44bb8df7b321c8d46fc1ebb76b558c291b4d0b4e
Asif Raza <asif.raza@azure-i.com>
2021-12-02 01:37:17 +0500

    phone

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
             'first_name' => 'required|string',
             'last_name' => 'required|string',
             'email' => 'required|email',
-            'phone' => 'required|string|unique:user_apps,phone',
+            'phone' => 'required|string',
             'password' => 'nullable|string',
             'role' => 'required|in:1,2',
         ];
