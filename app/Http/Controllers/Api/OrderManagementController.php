db5acbe4b179d2ad073fa43c39531f1b785b3be7
Asif Raza <asif.raza@azure-i.com>
2022-02-02 15:05:42 +0500

    fixes

--- app/Http/Controllers/Api/OrderManagementController.php
+++ app/Http/Controllers/Api/OrderManagementController.php
@@ -492,12 +492,13 @@ class OrderManagementController extends Controller
         }
 
         $links = [];
-        foreach($request->image as $image){
+        foreach($request->image as $key=>$image){
             // $image = $request->image;  // your base64 encoded
             $image = str_replace(' ', '+', $image);
             $imageName = Str::random(10).'.png';
             File::put(public_path('uploads/'.$imageName), base64_decode($image));
-            array_push($links, $imageName);
+            // array_push($links, $imageName);
+            $links[$key] = [$imageName];
         }
         return response()->json([
             'status' => true,
